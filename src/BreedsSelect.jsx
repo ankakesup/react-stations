f509fbdbf89f4d2892dc9992b4641ce7cec5d435
ankakesup <ankakesup@gmail.com>
2026-03-09 18:22:13 +0900

    done-station10

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,17 @@
 // @ts-check
 
-export const BreedsSelect = () => {
-  return <></>
+export const BreedsSelect = ({breeds, selectBreed, selectedBreed}) => {
+
+  return (
+    <>
+      {/* e.target.value でイベント(e)が起きた要素(target)のvalueを参照する */}
+      <select value={selectedBreed} id="petName" onChange={(e) => selectBreed(e.target.value)}> 
+        {breeds.map( elem => (
+          <option value={elem}>{elem}</option>
+        ))}
+      </select>
+    </>
+  )
 }
 
 export default BreedsSelect
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,22 +1,26 @@
 // @ts-check
 
 import { useEffect, useState } from "react"
+import BreedsSelect from "./BreedsSelect"
 
 export const DogListContainer = () => {
 
-  const [breeds, setBreeds] = useState()
+  const [breeds, setBreeds] = useState([])
+  const [selectedBreed, setSelectedBreed] = useState(0)
 
   useEffect(() =>{
     fetch("https://dog.ceo/api/breeds/list/all")
       .then(response => response.json())
-      .then(data => 
-        setBreeds(data.message)
-      )
+      .then(data => {
+        setBreeds(Object.keys(data.message))
+        console.log(data.message)
+      }
+    )
   }, [])
 
   return (
     <>
-
+      <BreedsSelect breeds={breeds} selectBreed={setSelectedBreed} selectedBreed={selectedBreed}/>
     </>
   )
 }
